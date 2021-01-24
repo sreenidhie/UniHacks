b996e7d183ba8a59e7c1efcc4dd460edc7d92935
Sreenidhi Elayaperumal <sreenidhi.elay@gmail.com>
2021-01-24 20:43:18 +0000

    everything

---
+++ College Search/script.js
@@ -0,0 +1,8 @@
+/* side nav bar */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
